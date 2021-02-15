39d6aacf604386a354d291f33b30acfde840a5b0
Harsh Rathod <55552474+thecreator-hr@users.noreply.github.com>
2021-02-15 11:41:21 +0530

    adding comments to 13

--- Lab1/src/q3.java
+++ Lab1/src/q3.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
 public class q3 {
+	
+	/*
+	 * Logic - 
+	 * Take in input from the user then in each iteration finding the units digit cubing it and adding it to another 
+	 * number then dividing the initial number by 10 till the number becomes 0
+	 * finally checking if the numbers inputted and the number obtained from the abouve algo are same or not
+	 * if yes then display armstrong else not armstrong
+	 */
 
 	public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int a = sc.nextInt();
 		int arm = 0, n = a;
