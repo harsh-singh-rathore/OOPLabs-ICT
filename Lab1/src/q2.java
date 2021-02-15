be0da526c49bb3dcd9808b0b7f5789cfb9d078ab
Harsh Rathod <55552474+thecreator-hr@users.noreply.github.com>
2021-02-15 11:33:44 +0530

    added comments to q2

--- Lab1/src/q2.java
+++ Lab1/src/q2.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 
 public class q2 {
 	
+	/*
+	 * Logic -
+	 * Take in inputs in main then call the calculator method giving it the arguments of two numbers and the operator
+	 * put the operator in the switch case where do the respective operations and if the char is not in +\-\*\/
+	 * skip the function and return -1 instead
+	 * method call is in a do while loop in main method where the method is called atleast once and will terminate if the
+	 * answer to the question Do you want to continue is given false.
+	 */
+	
 	static float calculator(float a, float b, char c) {
 		
 		switch(c)
