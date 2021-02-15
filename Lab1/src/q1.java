d34d90d6c0a9d40cc3ed9e2b8bf3e252dd757fb6
Harsh Rathod <55552474+thecreator-hr@users.noreply.github.com>
2021-02-15 11:26:21 +0530

    added comments to q1

--- Lab1/src/q1.java
+++ Lab1/src/q1.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class q1 {
 
+	/*
+	 * Logic -
+	 * Write a Java program to print table of number entered by user
+	 * Take in input then run a loop till 10 and multiply the i incremented at each iteration to the number
+	 * entered by the user then display it thus getting the desired output
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
